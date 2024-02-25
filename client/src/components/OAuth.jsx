edc3fc1192089f5da3bd8512d41c5d1869c19f68
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-02-25 17:13:26 +0530

    addd OAuth functionality

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,50 @@
+import { Button } from "flowbite-react";
+import { AiFillGoogleCircle } from "react-icons/ai";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
+
+const OAuth = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const auth = getAuth(app);
+  const handleGoogleClick = async () => {
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: "select_account" });
+    try {
+      const resultsForGoogle = await signInWithPopup(auth, provider);
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: resultsForGoogle.user.displayName,
+          email: resultsForGoogle.user.email,
+          googlePhotoUrl: resultsForGoogle.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Button
+      type="button"
+      gradientDuoTone="pinkToOrange"
+      onClick={handleGoogleClick}
+      outline
+    >
+      <AiFillGoogleCircle className="w-6 h-6 mr-2" />
+      Continue With Google
+    </Button>
+  );
+};
+
+export default OAuth;
