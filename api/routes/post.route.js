f7730b3bd24960ec25c099cb908b4270518c6fe3
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-02-27 15:51:44 +0530

    add delete post functionalily to dashboard

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,14 @@
 import expres from "express";
 import { verifyToken } from "../utils/veryfyUser.js";
-import { create, getposts } from "../controllers/post.controller.js";
+import {
+  create,
+  getposts,
+  deletepost,
+} from "../controllers/post.controller.js";
 
 const router = expres.Router();
 router.post("/create", verifyToken, create);
 router.get("/getposts", getposts);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
 
 export default router;
